8254f01779cce88aa243624ed598f1fb72f8c321
Emilie Montredon <emontredon@ekyno.com>
2016-08-16 18:07:55 +0200

    split the code to send an email from the code to send a specific reset password email

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 var Mailjet = require("node-mailjet");
 
 var mailjetAdapter = options => {
-  if (!options || !options.apiKey || !options.apiSecret || !options.fromAddress) {
+  if (!options || !options.apiKey || !options.apiSecret || !options.fromEmail) {
     throw "mailjetAdapter requires an API key, an API secret and a from address.";
   }
   var mailjet = Mailjet.connect(options.apiKey, options.apiSecret);
 
-  var sendMail = mail => {
+  /**
+   * @function sendPasswordResetEmail
+   * @description Sends the link to reset the password
+   */
+  var sendPasswordResetEmail = mail => {
     var send = mailjet.post("send");
 
     var data = {
-      "FromEmail": options.fromAddress,
+      "FromEmail": options.fromEmail,
       "FromName": options.fromName,
-      "Recipients": [{"Email": mail.to}]
+      "Recipients": [{"Email": mail.user.get("email")}]
     }
 
     // set the subject
-    var subject = options.subject;
+    var subject = options.passwordResetSubject;
     data["Subject"] = (subject ? subject : mail.subject);
 
     // use the template if any, and extract the link to set it
-    if (options.templateId) {
-      var idx = mail.text.indexOf(options.linkPattern);
-      var link = mail.text.substring(idx);
-
-      var variables = { "link_param": link };
-
+    if (options.passwordResetTemplateId) {
       data["MJ-TemplateLanguage"] = "true";
       data["MJ-TemplateErrorReporting"] = options.apiErrorEmail;
-      data["Mj-TemplateID"] = options.templateId;
-      data["Vars"] = variables;
+      data["Mj-TemplateID"] = options.passwordResetTemplateId;
+      data["Vars"] = {
+        "appName": mail.appName,
+        "link": mail.link
+      };
     }
     else {
       data["Text-part"] = mail.text;
@@ -40,7 +42,41 @@ var mailjetAdapter = options => {
     });
   }
 
+
+  /**
+   * @function sendVerificationEmail
+   * @description Sends the link to verify an email
+   */
+  // var sendVerificationEmail = mail => {
+  //   return new Promise((resolve, reject) => {
+  //     resolve();
+  //   });
+  // }
+
+
+  /**
+   * @function sendMail
+   * @description Sends a pre-defined email
+   */
+  var sendMail = mail => {
+    var send = mailjet.post("send");
+
+    var data = {
+      "FromEmail": options.fromEmail,
+      "FromName": options.fromName,
+      "Recipients": [{"Email": mail.to}],
+      "Subject": mail.subject,
+      "Text-part": mail.text
+    }
+
+    return new Promise((resolve, reject) => {
+      send.request(data).then(resolve).catch(reject);
+    });
+  }
+
   return Object.freeze({
+    // sendVerificationEmail: sendVerificationEmail,
+    sendPasswordResetEmail: sendPasswordResetEmail,
     sendMail: sendMail
   });
 }
